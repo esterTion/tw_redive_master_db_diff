739ea828c9d9b9297e8404ff13bf4e8766b8d037
esterTion-serveraccount <estertion@estertion.win>
2018-08-04 22:20:34 +0800

    00000000

---
+++ hatsune_unlock_story_condition.sql
@@ -0,0 +1,17 @@
+CREATE TABLE 'hatsune_unlock_story_condition' ('story_id' INTEGER NOT NULL, 'event_id' INTEGER NOT NULL, 'condition_entry' INTEGER NOT NULL, 'condition_quest_id' INTEGER NOT NULL, 'condition_boss_id' INTEGER NOT NULL, 'condition_mission_id' INTEGER NOT NULL, PRIMARY KEY('story_id'));
+INSERT INTO `hatsune_unlock_story_condition` VALUES (/*story_id*/5001000, /*event_id*/10001, /*condition_entry*/1, /*condition_quest_id*/0, /*condition_boss_id*/0, /*condition_mission_id*/0);
+INSERT INTO `hatsune_unlock_story_condition` VALUES (/*story_id*/5001001, /*event_id*/10001, /*condition_entry*/0, /*condition_quest_id*/61001003, /*condition_boss_id*/0, /*condition_mission_id*/0);
+INSERT INTO `hatsune_unlock_story_condition` VALUES (/*story_id*/5001002, /*event_id*/10001, /*condition_entry*/0, /*condition_quest_id*/61001005, /*condition_boss_id*/0, /*condition_mission_id*/0);
+INSERT INTO `hatsune_unlock_story_condition` VALUES (/*story_id*/5001003, /*event_id*/10001, /*condition_entry*/0, /*condition_quest_id*/61001007, /*condition_boss_id*/0, /*condition_mission_id*/0);
+INSERT INTO `hatsune_unlock_story_condition` VALUES (/*story_id*/5001004, /*event_id*/10001, /*condition_entry*/0, /*condition_quest_id*/61001009, /*condition_boss_id*/0, /*condition_mission_id*/0);
+INSERT INTO `hatsune_unlock_story_condition` VALUES (/*story_id*/5001005, /*event_id*/10001, /*condition_entry*/0, /*condition_quest_id*/61001012, /*condition_boss_id*/0, /*condition_mission_id*/0);
+INSERT INTO `hatsune_unlock_story_condition` VALUES (/*story_id*/5001006, /*event_id*/10001, /*condition_entry*/0, /*condition_quest_id*/61001015, /*condition_boss_id*/0, /*condition_mission_id*/0);
+INSERT INTO `hatsune_unlock_story_condition` VALUES (/*story_id*/5001007, /*event_id*/10001, /*condition_entry*/0, /*condition_quest_id*/0, /*condition_boss_id*/1000101, /*condition_mission_id*/0);
+INSERT INTO `hatsune_unlock_story_condition` VALUES (/*story_id*/5002000, /*event_id*/10002, /*condition_entry*/1, /*condition_quest_id*/0, /*condition_boss_id*/0, /*condition_mission_id*/0);
+INSERT INTO `hatsune_unlock_story_condition` VALUES (/*story_id*/5002001, /*event_id*/10002, /*condition_entry*/0, /*condition_quest_id*/61002003, /*condition_boss_id*/0, /*condition_mission_id*/0);
+INSERT INTO `hatsune_unlock_story_condition` VALUES (/*story_id*/5002002, /*event_id*/10002, /*condition_entry*/0, /*condition_quest_id*/61002005, /*condition_boss_id*/0, /*condition_mission_id*/0);
+INSERT INTO `hatsune_unlock_story_condition` VALUES (/*story_id*/5002003, /*event_id*/10002, /*condition_entry*/0, /*condition_quest_id*/61002007, /*condition_boss_id*/0, /*condition_mission_id*/0);
+INSERT INTO `hatsune_unlock_story_condition` VALUES (/*story_id*/5002004, /*event_id*/10002, /*condition_entry*/0, /*condition_quest_id*/61002009, /*condition_boss_id*/0, /*condition_mission_id*/0);
+INSERT INTO `hatsune_unlock_story_condition` VALUES (/*story_id*/5002005, /*event_id*/10002, /*condition_entry*/0, /*condition_quest_id*/61002012, /*condition_boss_id*/0, /*condition_mission_id*/0);
+INSERT INTO `hatsune_unlock_story_condition` VALUES (/*story_id*/5002006, /*event_id*/10002, /*condition_entry*/0, /*condition_quest_id*/61002015, /*condition_boss_id*/0, /*condition_mission_id*/0);
+INSERT INTO `hatsune_unlock_story_condition` VALUES (/*story_id*/5002007, /*event_id*/10002, /*condition_entry*/0, /*condition_quest_id*/0, /*condition_boss_id*/0, /*condition_mission_id*/71002025);
---
+++ room_item_announcement.sql
@@ -0,0 +1,2 @@
+CREATE TABLE 'room_item_announcement' ('id' INTEGER NOT NULL, 'announcement_start' TEXT NOT NULL, 'announcement_end' TEXT NOT NULL, PRIMARY KEY('id'));
+INSERT INTO `room_item_announcement` VALUES (/*id*/1, /*announcement_start*/"2018/06/15 15:00:00", /*announcement_end*/"2018/07/17 15:00:00");
