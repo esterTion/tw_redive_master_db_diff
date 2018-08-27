3e79d52250b07087a582a7721abb85c553fd27c4
esterTion-serveraccount <estertion@estertion.win>
2018-08-27 15:55:20 +0800

    00001005

--- odds_name_data.sql
+++ odds_name_data.sql
@@ -1,8 +1,6 @@
 CREATE TABLE 'odds_name_data' ('id' INTEGER NOT NULL, 'odds_file' TEXT NOT NULL, 'name' TEXT NOT NULL, 'icon_type' INTEGER NOT NULL, 'description' TEXT NOT NULL, PRIMARY KEY('id'));
-INSERT INTO `odds_name_data` VALUES (/*id*/1, /*odds_file*/"box_rank_1-3", /*name*/"ランダム装備BOX", /*icon_type*/99002, /*description*/"ランダムな装備が獲得できる。");
-INSERT INTO `odds_name_data` VALUES (/*id*/2, /*odds_file*/"eq_rank_3-6", /*name*/"RANK3～6の装備アイテム", /*icon_type*/99002, /*description*/"キャラのRANK1～4で必要となる装備アイテム");
-INSERT INTO `odds_name_data` VALUES (/*id*/3, /*odds_file*/"eq_rank_4-7", /*name*/"RANK4～7の装備アイテム", /*icon_type*/99002, /*description*/"キャラのRANK4～7で必要となる装備アイテム");
-INSERT INTO `odds_name_data` VALUES (/*id*/4, /*odds_file*/1000120101, /*name*/"ハツネのメモリーピース", /*icon_type*/99002, /*description*/"");
-INSERT INTO `odds_name_data` VALUES (/*id*/5, /*odds_file*/1000120102, /*name*/"シオリのメモリーピース", /*icon_type*/99002, /*description*/"");
-INSERT INTO `odds_name_data` VALUES (/*id*/6, /*odds_file*/1000220101, /*name*/"ミソギのメモリーピース", /*icon_type*/99002, /*description*/"");
-INSERT INTO `odds_name_data` VALUES (/*id*/7, /*odds_file*/1000220102, /*name*/"ミミのメモリーピース", /*icon_type*/99002, /*description*/"");
+INSERT INTO `odds_name_data` VALUES (/*id*/1, /*odds_file*/"box_rank_1-3", /*name*/"隨機裝備盒", /*icon_type*/99002, /*description*/"可隨機獲得裝備。");
+INSERT INTO `odds_name_data` VALUES (/*id*/2, /*odds_file*/"eq_rank_3-6", /*name*/"RANK3～6的裝備道具", /*icon_type*/99002, /*description*/"角色RANK3～6所需要的裝備道具");
+INSERT INTO `odds_name_data` VALUES (/*id*/3, /*odds_file*/"eq_rank_4-7", /*name*/"RANK4～7的裝備道具", /*icon_type*/99002, /*description*/"角色RANK4～7所需要的裝備道具");
+INSERT INTO `odds_name_data` VALUES (/*id*/4, /*odds_file*/1000120101, /*name*/"初音的記憶碎片", /*icon_type*/99002, /*description*/"");
+INSERT INTO `odds_name_data` VALUES (/*id*/5, /*odds_file*/1000120102, /*name*/"栞的記憶碎片", /*icon_type*/99002, /*description*/"");
